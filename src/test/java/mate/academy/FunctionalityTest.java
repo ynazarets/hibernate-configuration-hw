733b8bd8c92703fc286936567985d06fd7a0633a
Yehor Nazarets <nazarets1111@gmail.com>
2025-09-05 20:10:02 +0200

    I don`t know why

--- src/test/java/mate/academy/FunctionalityTest.java
+++ src/test/java/mate/academy/FunctionalityTest.java
@@ -129,7 +129,6 @@ public class FunctionalityTest extends AbstractTest {
             Mockito.when(mockedSession.beginTransaction()).thenReturn(mockedTransaction);
             mockSessionFactory(mockedSessionFactory);
             Object testMovie = getTestMovie();
-            Mockito.when(mockedSession.save(testMovie)).thenThrow(new RuntimeException());
             doThrow(new RuntimeException()).when(mockedSession).persist(testMovie);
             Class dataProcessingExceptionClass = getClass("DataProcessingException");
 
